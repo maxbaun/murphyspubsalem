780bea604997ca3d1689df5de516ec1dc0749bcb
Max Baun <maxbaun@gmail.com>
2016-05-10 14:15:09 -0400

    origin conflicts

--- libs/facebook-albums.php
+++ libs/facebook-albums.php
@@ -1,14 +1,14 @@
 <?php
-
-date_default_timezone_set('America/New_York');  
+header('Access-Control-Allow-Origin: *');
+date_default_timezone_set('America/New_York');
 require_once("facebook.php");
-// 
+//
  $config = array();
  $config['appId'] = '409416482513248';
  $config['secret'] = 'df012044a3b3003e0814ad2f03fed7ed';
-// 
+//
  $facebook = new Facebook($config);
-// 
+//
 
 
 try {
@@ -38,22 +38,22 @@ foreach($data as $album)
 			   $coverdata = $facebook->api($album['cover_photo']);
 			} catch ( Exception $e ) {
 			    continue;
-			}			
+			}
 		}
-			
+
 		else
 			$coverdata = "";
 		// debugging
 		// var_dump($coverdata['images']);
 		// echo '<br/><br/>';
 		if(!isset($coverdata['images'][3])){
-			$cover = $coverdata['images'][1]['source'];	// if the regular image is not avail, use the first one			
+			$cover = $coverdata['images'][1]['source'];	// if the regular image is not avail, use the first one
 		}
 		else
 			$cover = $coverdata['images'][3]['source'];
 	}
 	$link = $album['link'];
-	
+
 	$albums[] = Array("id"=>$id,"name"=>$name,"cover"=>$cover,"link"=>$link);
 
 }
